34411b97ca9e04dc59225dc70efc6c8291ef26f0
Brandon Horst <brandonhorst@gmail.com>
2016-03-25 14:16:46 -0400

    Don't clobber other processor additions

--- src/processor.jsx
+++ src/processor.jsx
@@ -8,41 +8,40 @@ export default function createProcessor (extensions) {
     if (_.isString(element)) return element
 
     const theseExtensions = _.chain(extensions)
-      .filter((Extension) => _.includes(Extension.extends, element.type))
-      .map(Extension => (
-        <Extension {...element.props} id={undefined}>
-          {element.children}
-        </Extension>
-      ))
+      .filter((extension) => _.includes(extension.extends, element.type))
       .map((extension) => {
-        let result = extension
+        const newProps = _.assign({}, element.props, {id: undefined})
+        return _.assign({}, element, {type: extension}, {props: newProps})
+      })
+      .map((extension) => {
+        let final = extension
 
         if (element.type.mapResult) {
           function map (option) {
             const result = element.type.mapResult(option.result, extension)
             return _.assign({}, option, {result})
           }
-          result = <map outbound={map} skipIncomplete>{result}</map>
+          final = <map outbound={map} skipIncomplete>{final}</map>
         }
         if (element.type.filterResult) {
           function filter (option) {
             return element.type.filterResult(option.result, extension)
           }
-          result = <filter outbound={filter} skipIncomplete>{result}</filter>
+          final = <filter outbound={filter} skipIncomplete>{final}</filter>
         }
 
-        return result
+        return final
       })
       .value()
 
     if (theseExtensions.length) {
-      const NewPhrase = _.clone(element.type) // to prevent duplicate extension
+      const newPhrase = _.clone(element.type) // to prevent duplicate extension
+      const newProps = _.assign({}, element.props, {id: undefined})
+      const newElement = _.assign({}, element, {type: newPhrase}, {props: newProps})
       function describe (model) {
         return (
           <choice>
-            <NewPhrase {...element.props} id={undefined}>
-              {element.children}
-            </NewPhrase>
+            {newElement}
             {theseExtensions}
           </choice>
         )
